22d91d3d580e6a8bf1022957a27bb0b82b19f5f8
Assam Toumi <assamtomi@outlook.com>
2020-07-06 19:45:49 +0100

    done!

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LINQ2XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xdoc = XDocument.Load("database\\Products.xml");
+
+            xdoc.Descendants("product").Where(p => p.Element("name").Value.Contains 
+            ("Apple")).Select(p => new 
+                {
+                    id= p.Attribute("id").Value,
+                    name= p.Element("name").Value,
+                    price= p.Element("Price").Value,
+                    currency= p.Element("Price").Attribute("currency").Value
+                }).ToList().ForEach(p =>
+                {
+                    Console.WriteLine("Id:" + p.id);
+                    Console.WriteLine("name:" + p.name);
+                    Console.WriteLine("Price:" + p.price);
+                    Console.WriteLine("currency:" + p.currency);
+                    Console.WriteLine("***********");
+                });
+            /*
+                xdoc.Descendants("product").Select(p => (
+                id= p.Attribute("id").Value,
+                name= p.Element("name").Value,
+                price= p.Element("Price").Value,
+                currency= p.Element("Price").Attribute("currency").Value
+            )).ToList().ForEach(p => {
+                Console.WriteLine("Id:" + p.id);
+                Console.WriteLine("name:" + p.name);
+                Console.WriteLine("Price:" + p.price);
+                Console.WriteLine("currency:" + p.currency);
+                 Console.WriteLine("***********");
+                
+            });
+            */
+            // using conditions
+
+            Console.ReadLine();
+        }
+    }
+}
